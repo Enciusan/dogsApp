291357d232ebbe3656f099e070a99a4aa27c2e63
Enciusan <enciulescu.alexandru@gmail.com>
2024-09-19 18:25:20 +0300

    worked on matching cards

--- frontend/app/(tabs)/Dogs.tsx
+++ frontend/app/(tabs)/Dogs.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, View } from "react-native";
+import { Pressable, SafeAreaView, View } from "react-native";
 import { useAccountStore } from "../../store/account";
 import { getProfile } from "../../utils/function";
 import { supabase } from "../../utils/supa";
 import { Session } from "@supabase/supabase-js";
 import SwipeableCard from "../../components/SwipeableCard";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { UserType } from "../../types/account";
 
 export default function DogScreen() {
   const [session, setSession] = useState<Session | null>(null);
   const { accountInformation, updateAccountInformation } = useAccountStore((state) => state);
   const [isAccountInformationLoaded, setIsAccountInformationLoaded] = useState<boolean>(false);
+  const [dislikePressed, setDislikePressed] = useState<boolean>(false);
+  const [likePressed, setLikePressed] = useState<boolean>(false);
+  const [appUsers, setAppUsers] = useState<Array<Record<any, UserType>>>([]);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -19,6 +24,7 @@ export default function DogScreen() {
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
     });
+    getAllAccounts();
   }, []);
 
   useEffect(() => {
@@ -31,12 +37,37 @@ export default function DogScreen() {
           dogName: userProfile.dog_name,
         });
       }
+      console.log("App Users", appUsers);
     })();
   }, [session]);
 
   useEffect(() => {
     handleAccountInformation();
   }, [accountInformation.avatarUrl]);
+
+  const handleDislikePressed = () => {
+    setDislikePressed(true);
+  };
+
+  const handleLikePressed = () => {
+    setLikePressed(true);
+  };
+
+  const getAllAccounts = () => {
+    console.log("Session", session?.user?.id);
+    supabase
+      .from("profiles")
+      .select("*")
+      .neq("id", session?.user?.id)
+      .then(({ data, error }) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log("Data", data);
+          setAppUsers(data);
+        }
+      });
+  };
 
   const handleAccountInformation = () => {
     if (
@@ -54,7 +85,35 @@ export default function DogScreen() {
   return (
     <SafeAreaView className="h-screen bg-[#0E1514]">
       <View className="h-28"></View>
-      {isAccountInformationLoaded && <SwipeableCard url={accountInformation.avatarUrl} />}
+      {isAccountInformationLoaded && (
+        <>
+          {appUsers.map((user) => {
+            return (
+              <SwipeableCard
+                url={user.avatar_url}
+                dislikePressed={dislikePressed}
+                likePressed={likePressed}
+                setLikePressed={setLikePressed}
+                setDislikePressed={setDislikePressed}
+                username={user.username}
+                dogName={user.dog_name}
+              />
+            );
+          })}
+        </>
+      )}
+      <View className="flex flex-row justify-center items-center gap-10 pt-10">
+        <Pressable onPress={() => handleDislikePressed()}>
+          <View className="w-20 h-20 rounded-full bg-[#263b44] flex justify-center items-center">
+            <FontAwesome name="close" size={40} className="!text-rose-400 " />
+          </View>
+        </Pressable>
+        <Pressable onPress={() => handleLikePressed()}>
+          <View className="w-20 h-20 rounded-full bg-[#263b44] flex justify-center items-center">
+            <AntDesign name="heart" size={40} className="!text-emerald-400 pt-1" />
+          </View>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
--- frontend/components/SwipeableCard.tsx
+++ frontend/components/SwipeableCard.tsx
@@ -1,86 +1,83 @@
 import { Dimensions, Image, Pressable, Text, View } from "react-native";
-import React, { useState } from "react";
+import React from "react";
 import { useDownloadImage } from "../utils/hooks";
-import { AntDesign, FontAwesome } from "@expo/vector-icons";
-import Animated, { withDelay, withTiming } from "react-native-reanimated";
+import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from "react-native-reanimated";
+import { LinearGradient } from "expo-linear-gradient";
+import { CustomText } from "./CustomText";
 
 type Props = {
   url: string | null;
+  likePressed: boolean;
+  setLikePressed: (value: boolean) => void;
+  dislikePressed: boolean;
+  setDislikePressed: (value: boolean) => void;
+  username: string;
+  dogName: string;
 };
 
-const windowWidth = Dimensions.get("window").width;
-console.log("windowWidth", windowWidth);
-const customEntering = () => {
-  "worklet";
-  const animations = {
-    originX: withTiming(50, { duration: 3000 }),
-    opacity: withTiming(1, { duration: 2000 }),
-  };
-  const initialValues = {
-    originX: 50,
-    opacity: 0,
-  };
-  return {
-    initialValues,
-    animations,
-  };
-};
+const mobileWidth = Dimensions.get("window").width;
 
-const customExiting = () => {
-  "worklet";
-  const animations = {
-    originX: withTiming(2 * windowWidth, { duration: 3000 }),
-    opacity: withTiming(0, { duration: 2000 }),
-    transform: [{ scale: withTiming(0.2, { duration: 3500 }) }],
-  };
-  const initialValues = {
-    originX: 50,
-    opacity: 1,
-    transform: [{ scale: 1 }],
-  };
-  return {
-    initialValues,
-    animations,
-  };
-};
-
-export default function SwipeableCard({ url }: Props) {
+export default function SwipeableCard({
+  url,
+  dislikePressed,
+  setDislikePressed,
+  likePressed,
+  setLikePressed,
+  dogName,
+  username,
+}: Props) {
   const { avatarUrl } = useDownloadImage(url);
-  const [show, setShow] = useState<boolean>(false);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const handleLikePressed = () => {
+    translateX.value = withTiming(mobileWidth, { duration: 1500 });
+    setTimeout(() => {
+      setLikePressed(false);
+    }, 1500);
+  };
+
+  const handleDislikePressed = () => {
+    translateX.value = withTiming(-mobileWidth, { duration: 1500 });
+    setTimeout(() => {
+      setDislikePressed(false);
+    }, 1500);
+  };
+
+  if (dislikePressed) {
+    handleDislikePressed();
+  } else if (likePressed) {
+    handleLikePressed();
+  }
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: withSpring(translateX.value * 2) }],
+  }));
 
   // @ts-ignore
   return (
     <View className="flex items-center justify-center ">
       {avatarUrl ? (
         <>
-          {show ? (
-            <Animated.View
-              className="h-[500px] w-3/4 border-2 rounded-xl border-slate-500"
-              entering={customEntering}
-              exiting={customExiting}>
-              <Image source={{ uri: avatarUrl }} style={{ width: "100%", height: 497 }} className="rounded-xl" />
-            </Animated.View>
-          ) : (
-            <View className="h-[500px]" />
-          )}
+          <Animated.View className="h-[500px] w-3/4 border-2 rounded-xl border-slate-500" style={[animatedStyles]}>
+            <LinearGradient
+              colors={["transparent", "#0E1514"]}
+              style={{ width: "100%", height: 497, borderRadius: 10 }}
+            />
+            <Image
+              source={{ uri: avatarUrl }}
+              style={{ width: "100%", height: 497 }}
+              className="rounded-xl absolute !-z-10"
+            />
+            <CustomText type={"semiBold"} className="absolute bottom-6 left-5 text-lg text-center !text-slate-200">
+              I&apos;m {dogName} and my owner is {username}
+            </CustomText>
+          </Animated.View>
         </>
       ) : (
         <View>
           <Text className="text-slate-300">Loading...</Text>
         </View>
       )}
-      <View className="flex flex-row gap-10 pt-10">
-        <Pressable onPress={() => setShow(false)}>
-          <View className="w-20 h-20 rounded-full bg-[#263b44] flex justify-center items-center">
-            <FontAwesome name="close" size={40} className="!text-rose-400 " />
-          </View>
-        </Pressable>
-        <Pressable onPress={() => setShow(true)}>
-          <View className="w-20 h-20 rounded-full bg-[#263b44] flex justify-center items-center">
-            <AntDesign name="heart" size={40} className="!text-emerald-400 pt-1" />
-          </View>
-        </Pressable>
-      </View>
     </View>
   );
 }
